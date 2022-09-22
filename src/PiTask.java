a080e4781cc48b4b79c7cf3a0d2c5b267e78ba96
abdurrahman23ye <abdurrahman.kursicin@gmail.com>
2022-09-22 19:05:32 +0300

    odd number

---
+++ src/PiTask.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PiTask {
+
+    public static void main(String[] args) {
+
+
+        int min=15;
+        int max=51;
+
+
+        while(min<=max){  if(min%2!=0){System.out.println("Odd numbers = " + min);}
+            min++;}
+    }
+}
